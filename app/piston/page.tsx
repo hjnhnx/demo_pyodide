e626ed065561fe55c4555cdc07831e9e4c31afbc
newit-hinhnx <hinhnx@newit.co.jp>
2024-04-23 17:16:12 +0700

    Fix | set width to 65%

--- app/piston/page.tsx
+++ app/piston/page.tsx
@@ -2,7 +2,7 @@ import CodeEditor from "@/components/CodeEditor";
 
 export default function Piston() {
     return (
-        <div className="min-h-[100vh] px-6 py-8 bg-[#0f0a19]">
+        <div className="min-h-[100vh] px-6 py-8 bg-[#0f0a19] w-[65vw] m-auto">
             <CodeEditor />
         </div>
     );
